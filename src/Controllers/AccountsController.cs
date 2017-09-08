af1a67c4f77de0b29d10c3b65edc66882929210b
shaun <shaun@cmune.com>
2017-09-08 15:43:36 -0700

    Implemented email authentication and ability for existing accounts to update their email and password.

---
+++ src/Controllers/AccountsController.cs
@@ -0,0 +1,94 @@
+﻿using HappyTokenApi.Data.Core;
+using HappyTokenApi.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace HappyTokenApi.Controllers
+{
+    [Route("[controller]")]
+    public class AccountsController : Controller
+    {
+        private readonly CoreDbContext m_CoreDbContext;
+
+        public AccountsController(CoreDbContext coreDbContext)
+        {
+            m_CoreDbContext = coreDbContext;
+        }
+
+        [AllowAnonymous]
+        [HttpPost("email", Name = nameof(AuthByEmail))]
+        public async Task<IActionResult> AuthByEmail([FromBody] UserEmailLogin userEmailLogin)
+        {
+            // Is the email data valid?
+            if (string.IsNullOrEmpty(userEmailLogin?.Email) || string.IsNullOrEmpty(userEmailLogin.Password))
+            {
+                return BadRequest("User authentication data was null or empty.");
+            }
+
+            // Pull the users data from the DB
+            var dbUser = await m_CoreDbContext.Users
+                .Where(dbu => dbu.Email == userEmailLogin.Email && dbu.Password == userEmailLogin.Password)
+                .SingleOrDefaultAsync();
+
+            if (dbUser != null)
+            {
+                var response = new UserAuthPair
+                {
+                    UserId = dbUser.UserId,
+                    AuthToken = dbUser.AuthToken
+                };
+
+                return Ok(response);
+            }
+
+            return BadRequest("Could not authenticate user by email.");
+        }
+
+        [HttpPost("email/{userId}", Name = nameof(LinkEmail))]
+        public async Task<IActionResult> LinkEmail(string userId, [FromBody] UserEmailLogin userEmailLogin)
+        {
+            if (string.IsNullOrEmpty(userId))
+            {
+                return BadRequest("UserId was null or empty.");
+            }
+
+            if (string.IsNullOrEmpty(userEmailLogin?.Email) || string.IsNullOrEmpty(userEmailLogin.Password))
+            {
+                return BadRequest("User email data was null or empty.");
+            }
+
+            // If the claimant userId matches the request userId, we allow the update
+            if (!this.IsClaimantUserId(userId))
+            {
+                return BadRequest("UserId was invalid for this request.");
+            }
+
+            // Pull the users data from the DB
+            var dbUser = await m_CoreDbContext.Users
+                .Where(dbu => dbu.UserId == userId)
+                .SingleOrDefaultAsync();
+
+            if (dbUser != null)
+            {
+                dbUser.Email = userEmailLogin.Email;
+                dbUser.Password = userEmailLogin.Password;
+
+                await m_CoreDbContext.SaveChangesAsync();
+
+                var response = new RequestResult
+                {
+                    Content = "Successfully updated email.",
+                    StatusCode = 0
+                };
+
+                return Ok(response);
+            }
+
+            return BadRequest("Could not link email to user.");
+        }
+    }
+}

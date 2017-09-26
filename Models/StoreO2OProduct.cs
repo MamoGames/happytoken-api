c206b0d736fcb264b5f3dc5cf932b017ef8900e6
Raymond Chan <raymond.chan@mamogames.com>
2017-09-26 11:21:24 +0800

    added O2O product model and flag in Store to disable O2O when needed

---
+++ Models/StoreO2OProduct.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using HappyTokenApi.Models;
+
+namespace HappyTokenApi
+{
+    public class StoreO2O : StoreProduct
+    {
+        public string ProductCode { get; set; }
+
+        public string VendorProductCode { get; set; }
+
+        public int Inventory { get; set; }
+    }
+}
